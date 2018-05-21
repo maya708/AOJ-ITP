7a2a884a2c055cf903e17ea32538387c55201c4b
maya708 <s1260186@u-aizu.ac.jp>
2018-05-21 17:47:36 +0900

    Swapping Two Numbers

---
+++ ITP1_3/topic3c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	while(1){
+		scanf("%d %d",&a,&b);
+		if(a==b && a==0){
+			break;
+		}else if(a>b){
+			printf("%d %d\n",b,a);
+		}else{
+			printf("%d %d\n",a,b);
+		}
+	}
+}
